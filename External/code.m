5c8f3bf18fb0936c6c7efd689aada0b8626a260e
alxhslm <alexhaslam12@gmail.com>
2020-03-20 12:42:24 +0000

    Fix for VS Code function on MacOS

--- External/code.m
+++ External/code.m
@@ -1,4 +1,4 @@
-function vscode(varargin)
+function code(varargin)
 if ~nargin
     docs = matlab.desktop.editor.getAll;
     files = sprintf('"%s" ',docs.Filename);
@@ -12,7 +12,7 @@ end
 if strcmp(computer,'PCWIN64')
     exe = '"C:\Program Files\Microsoft VS Code\Code.exe"';
 elseif strcmp(computer,'MACI64')
-    exe = 'open -a /Applications/Microsoft VS Code.app';
+    exe = 'open -a /Applications/Visual\ Studio\ Code.app';
 end
 
 system([exe ' ' files]);
