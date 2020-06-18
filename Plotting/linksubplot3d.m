5a00568287bc155df2004b29e2f63f1955c73d45
alxhslm <alexhaslam12@gmail.com>
2020-06-18 14:48:59 +0100

    Move to ARES repo

--- Plotting/linksubplot3d.m
+++
@@ -1,6 +0,0 @@
-function linksubplot3d(ax)
-link.xy = linkprop(ax(:),{'xlim','ylim'});
-link.mag = linkprop(ax(1,:),'clim');
-link.phase = linkprop(ax(2,:),'clim');
-fig = get(ax(1),'Parent');
-setappdata(fig,'links',link)
